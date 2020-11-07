aa40971b2e375cea7d0f5d292f5ef370879b504e
vkartik2k <vkartik2k@gmail.com>
2020-11-07 19:04:07 +0530

    course enrolled

--- models/user.js
+++ models/user.js
@@ -59,60 +59,9 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
          ref: "Receipt"
     }],
-    coursesEnrolled:[{ 
-        course:{   
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Course"
-        },
-        feesPaid:{
-            type:Boolean,
-            default:false
-        }
-    }],
     teacherAccessCourses:[{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Course"
-    }],
-    assignmentsDone:[{
-        assignment:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Assignment"
-        },
-        marksScored:{
-            type:Number,
-            required:[true, 'Please fill the maximum marks of test!'],
-        },
-        attemptsLeft:{
-            type:Number,
-            default:3,
-            required:[true, 'Please fill the maximum marks of test!'],
-        }
-    }],
-    testsDone:[{
-        test:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Test"
-        },
-        marksScored:{
-            type:Number,
-            required:[true, 'Please fill the maximum marks of test!'],
-        },
-        responseSheet:{
-            type: mongoose.Schema.Types.ObjectId,
-              ref: "ResponseSheet"   
-        }
-    }],
-    lecturesDone:[{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Lecture"
-    }],
-    topicsDone:[{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Topic"
-    }],
-    coursesDone:[{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Course"
     }],
     createdAt: { type: Date, default: Date.now },
 })
